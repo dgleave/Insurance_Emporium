887754018c9b6342fcc7ec64e5938d0f7131c7fa
Gleave <david.gleave@balfourbeatty.com>
2022-07-17 20:19:31 +0100

    task/Create_Carousel: Updated images. Removed uneeded Console Logs

--- AdvertBannerScript.js
+++ AdvertBannerScript.js
@@ -7,14 +7,13 @@ const navButtonsArray = Array.from(navButtons.children);
 const slideWidth = slides[0].getBoundingClientRect().width;
 const showAllButton = document.querySelector('.show_all_button');
 const showAllSlides = document.querySelector('.gridContainer');
-const gridSlidesArray = Array.from(showAllSlides.children);
 
 
 slides.forEach((slide, index)=>{        //Positions slides next to each other rather than on top of each other
     slide.style.left = slideWidth * index + 'px';
 })
 
-const goToSlide = (slideList, currentSlide, targetSlide) => {
+const goToSlide = (slideList, currentSlide, targetSlide) => {                       //used to determine which slide is to be navigated to when button clicked
     slideList.style.transform = 'translateX(-' + targetSlide.style.left; + ')';
     currentSlide.classList.remove('current-slide');
     targetSlide.classList.add('current-slide');
@@ -25,7 +24,6 @@ rightButton.addEventListener('click', e => {
     let currentSlide = slideList.querySelector('.current-slide');     //gets current slide being viewed
     const nextSlide = currentSlide.nextElementSibling;                          //gets next slide in the list
     goToSlide(slideList, currentSlide, nextSlide);
-
     const currentButton = navButtons.querySelector('.current-slide');
     const clickedButton = currentButton.nextElementSibling;
     updateButtons(currentButton, clickedButton);
@@ -41,10 +39,9 @@ leftButton.addEventListener('click',e => {
     updateButtons(currentButton, clickedButton);
 })
 
-navButtons.addEventListener('click', e =>{
+navButtons.addEventListener('click', e =>{                      //using the circular buttons underneath the carousel to navigate through the banners
     const clickedButton = e.target.closest('button');
     if(!clickedButton) return;
-
     const currentSlide = slideList.querySelector('.current-slide');
     const currentButton = navButtons.querySelector('.current-slide');
     const targetButtonIndex = navButtonsArray.findIndex(btn => btn === clickedButton);
@@ -55,13 +52,12 @@ navButtons.addEventListener('click', e =>{
     updateButtons(currentButton, clickedButton);
 })
 
-const updateButtons = (currentButton, clickedButton) => {
-    console.log('clicked button ',clickedButton);
+const updateButtons = (currentButton, clickedButton) => {           //Making sure visual effect of the buttons update
     currentButton.classList.remove('current-slide');
     clickedButton.classList.add('current-slide');
 }
 
-showAllButton.addEventListener('click', e =>{
+showAllButton.addEventListener('click', e =>{           //button to show all banners in  a single screen
     if(showAllSlides.style.visibility === "visible"){
         showAllSlides.style.visibility = "hidden";
     } else{
@@ -69,9 +65,8 @@ showAllButton.addEventListener('click', e =>{
     }
 })
 
-showAllSlides.addEventListener('click', e =>{
+showAllSlides.addEventListener('click', e =>{               //select an item on the show all page to jump to
     const clickedGrid = e.target.id;
-    console.log(clickedGrid);
     const currentSlide = slideList.querySelector('.current-slide');
     const targetButtonIndex = slides[clickedGrid];
     goToSlide(slideList, currentSlide, targetButtonIndex);
