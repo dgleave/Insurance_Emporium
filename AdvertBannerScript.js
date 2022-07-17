ad313ab5412989cc7741eca8cdf8009906721916
Gleave <david.gleave@balfourbeatty.com>
2022-07-17 14:05:06 +0100

    rask/Create_Carousel: create working left and right buttons

--- AdvertBannerScript.js
+++ AdvertBannerScript.js
@@ -1,15 +1,31 @@
+const slideList = document.querySelector('.carousel_slide_container_list');
+const slides = Array.from(slideList.children);
+const leftButton = document.querySelector('.carousel_button--left');
+const rightButton = document.querySelector('.carousel_button--right');
+const navButtons = document.querySelector('.carousel_indicator');
+const navButtonsArray = Array.from(navButtons.children);
 
-// function createCarouselBtn() {
-//     const itemList = document.getElementsByClassName("carousel_slide_container_list");
-//     let btn = document.createElement("button");
-//     const listLength = document.getElementById("myItems").getElementsByTagName("li").length;
-//     var buttonList = document.createDocumentFragment();
-//     for (i = 0; i < listLength; i++) {
-//         buttonList.appendChild(btn);
-//         //document.getElementById("carouselIndicator").appendChild(btn);
-//         console.log(i);
-//     }
-//     document.getElementById("carouselIndicator").appendChild(buttonList);
-// }
+const slideWidth = slides[0].getBoundingClientRect().width;
 
 
+slides.forEach((slide, index)=>{        //Positions slides next to each other rather than on top of each other
+    slide.style.left = slideWidth * index + 'px';
+})
+
+const goToSlide = (slideList, currentSlide, targetSlide) => {
+    slideList.style.transform = 'translateX(-' + targetSlide.style.left; + ')';
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
+
+rightButton.addEventListener('click', e => {
+    let currentSlide = slideList.querySelector('.current-slide');     //gets current slide being viewed
+    const nextSlide = currentSlide.nextElementSibling;                          //gets next slide in the list
+    goToSlide(slideList, currentSlide, nextSlide);
+})
+
+leftButton.addEventListener('click',e => {
+    const currentSlide = slideList.querySelector('.current-slide');     //gets current slide being viewed
+    const lastSlide = currentSlide.previousElementSibling;                          //gets next slide in the list
+    goToSlide(slideList, currentSlide, lastSlide);
+})
