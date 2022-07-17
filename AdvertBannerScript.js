50a157f54bd340c65b16aabc69895cf894a4481a
Gleave <david.gleave@balfourbeatty.com>
2022-07-17 12:57:58 +0100

    task/Create_Carousel: created basic framework and positioning with 4 banners

---
+++ AdvertBannerScript.js
@@ -0,0 +1,15 @@
+
+// function createCarouselBtn() {
+//     const itemList = document.getElementsByClassName("carousel_slide_container_list");
+//     let btn = document.createElement("button");
+//     const listLength = document.getElementById("myItems").getElementsByTagName("li").length;
+//     var buttonList = document.createDocumentFragment();
+//     for (i = 0; i < listLength; i++) {
+//         buttonList.appendChild(btn);
+//         //document.getElementById("carouselIndicator").appendChild(btn);
+//         console.log(i);
+//     }
+//     document.getElementById("carouselIndicator").appendChild(buttonList);
+// }
+
+
